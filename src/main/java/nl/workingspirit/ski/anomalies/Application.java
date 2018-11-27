be1c837ca376a66dc6163d3d9b2343956ef02429
Raymond Loman <raymond@carpago.nl>
2018-11-27 09:35:04 +0100

    Add binary search. Add anomalies regarding List and boxing

---
+++ src/main/java/nl/workingspirit/ski/anomalies/Application.java
@@ -0,0 +1,27 @@
+package nl.workingspirit.ski.anomalies;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Application {
+   
+   public static void main(String[] args) {
+      int[] numbers = {1,2,3};
+      
+//      List<Integer> integerList =  Arrays.asList(numbers); // compiler error since numbers is int[] and boxing fails
+      
+      List<Integer> numberList = Arrays.asList(0,1,1,2,3);
+      
+      Integer[] array = numberList.toArray(new Integer[0]);
+      
+      
+      for(int element: array) {
+         System.out.println(element);
+      }
+      
+      
+   }
+   
+
+}
---
+++ src/main/java/nl/workingspirit/ski/binarysearch/solution/Application.java
@@ -0,0 +1,22 @@
+package nl.workingspirit.ski.binarysearch.solution;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Application {
+   
+   public static void main(String[] args) {
+      
+      List<Integer> numbers = Arrays.asList(0,1,1,2,3,5,8,13,21,34,55,89);
+      
+      int result = Arrays.binarySearch(numbers.toArray(new Integer[0]), 34);
+      
+      System.out.println(result);
+      
+      result = Arrays.binarySearch(numbers.toArray(new Integer[0]), 45);
+      
+      System.out.println(result);
+      
+   }
+
+}

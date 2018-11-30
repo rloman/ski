77ac2fc8a9d482f47be33e2bdfc80fa2fdd7ca12
Raymond Loman <raymond@carpago.nl>
2018-11-30 12:27:05 +0100

    Add some more body to the template::Animal

--- src/main/java/nl/workingspirit/ski/template/Animal.java
+++ src/main/java/nl/workingspirit/ski/template/Animal.java
@@ -1,9 +1,26 @@
 package nl.workingspirit.ski.template;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public abstract class Animal {
    
    private String name;
+   private LocalDate birthDate;
+   
+   public Animal(String name, LocalDate birthDate) {
+      this.name = name;
+      this.birthDate = birthDate;
+   }
+   
+   public Animal() {
+      this.name = "Mickey - our cat";
+      this.birthDate = LocalDate.of(2011, 3, 9);
+   }
+   
+   public long getAge() {
+      return ChronoUnit.YEARS.between(this.birthDate,  LocalDate.now());
+   }
    
    public abstract void makeSound();
 

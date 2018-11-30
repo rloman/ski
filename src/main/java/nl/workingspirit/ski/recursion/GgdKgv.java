a67ae5b073d71b3e7b43262b93463d666a7a5ad6
Raymond Loman <raymond@carpago.nl>
2018-11-30 16:29:42 +0100

    Add GGD and KGV

---
+++ src/main/java/nl/workingspirit/ski/recursion/GgdKgv.java
@@ -0,0 +1,20 @@
+package nl.workingspirit.ski.recursion;
+
+
+public class GgdKgv {
+   
+   // can explain how and why but this is THE algorithm to calculate ggd
+   public static int ggd(int n, int m) {
+      if(n %m == 0){
+         return m;
+      }
+      else {
+         return ggd(m, n%m);
+      }
+   }
+   
+   public static int kgv(int n, int m) {
+      return (n*m)/ggd(n, m); // :-)
+   }
+
+}

fc93b47a45f89b132952f421f8c6dc89b6bc18ce
Raymond Loman <raymond@carpago.nl>
2018-11-30 18:29:09 +0100

    Add RecursiveList including a lot of methods which are all recursive

--- src/main/java/nl/workingspirit/ski/recursion/RecursiveList.java
+++ src/main/java/nl/workingspirit/ski/recursion/RecursiveList.java
@@ -16,14 +16,76 @@ public class RecursiveList <T> {
       }
    }
    
-   @Override
-   public String toString() {
-      String result = "";
-      if(this.head == null) {
-         return result;
+   public T get(int index) {
+      if(index == 0) {
+         return head;
       }
       else {
-         return result+head+tail;
+         return tail.get(--index);
+      }
+   }
+   
+   public boolean contains(T t) {
+      if(this.head == null) {
+         return false;
+      }
+      else {
+         if(this.head.equals(t)) {
+            return true;
+         }
+         else {
+            return tail.contains(t);
+         }
+      }
+      
+   }
+   
+   public void update(T victim, T target) {
+      if(this.head == null) {
+         return;
+      }
+      else {
+         if(this.head.equals(victim)) {
+            this.head = target;
+         }
+         else {
+            tail.update(victim, target);
+         }
+      }
+   }
+   
+   // hardest
+   public void delete(T victim) {
+      if(this.head == null) {
+         return;
+      }
+      else {
+         if(this.head.equals(victim)) {
+            this.head = tail.head;
+            this.tail = tail.tail;
+         }
+         else {
+            tail.delete(victim);
+         }
+      }
+   }
+   
+   public int size() {
+      if(this.head == null) {
+         return 0;
+      }
+      else {
+         return 1+tail.size();
+      }
+   }
+   
+   @Override
+   public String toString() {
+      if(this.head == null) {
+         return "";
+      }
+      else {
+         return ""+head+tail;
       }
    }
 

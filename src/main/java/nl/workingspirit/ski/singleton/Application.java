24e483434089889201450b0567efb201b6e9da8a
Raymond Loman <raymond@carpago.nl>
2018-11-30 11:49:48 +0100

    Add Design pattern: Singleton

---
+++ src/main/java/nl/workingspirit/ski/singleton/Application.java
@@ -0,0 +1,20 @@
+package nl.workingspirit.ski.singleton;
+
+
+public class Application {
+
+   public static void main(String[] args) {
+      
+      Singleton s = Singleton.getInstance();
+      
+      Singleton s2 = Singleton.getInstance();
+      
+      if(s != s2) {
+         // should never, ever come here!
+         System.out.println(0/0);
+      }
+      System.out.println("Successful completion");
+
+   }
+
+}
---
+++ src/main/java/nl/workingspirit/ski/singleton/Singleton.java
@@ -0,0 +1,19 @@
+package nl.workingspirit.ski.singleton;
+
+
+public class Singleton {
+   
+   private static Singleton instance;
+   
+   private Singleton() {}
+   
+   
+   public static synchronized Singleton getInstance() {
+      if(instance == null) {
+         instance = new Singleton();
+      }
+      return instance;
+   }
+   
+
+}

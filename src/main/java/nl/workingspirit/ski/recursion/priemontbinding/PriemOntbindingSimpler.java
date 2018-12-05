717ea2205f7eebf4c1a1bd496c53027c5e84e726
Raymond Loman <raymond@carpago.nl>
2018-12-05 10:42:09 +0100

    And even simpler PriemOntbindingSimpler

--- src/main/java/nl/workingspirit/ski/recursion/priemontbinding/PriemOntbindingSimpler.java
+++ src/main/java/nl/workingspirit/ski/recursion/priemontbinding/PriemOntbindingSimpler.java
@@ -1,7 +1,6 @@
 package nl.workingspirit.ski.recursion.priemontbinding;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class PriemOntbindingSimpler {
@@ -17,26 +16,29 @@ public class PriemOntbindingSimpler {
 
    private List<Integer> po(int lastFactor, int n) {
 
+      List<Integer> result = new ArrayList<>();
 
       //simplest case!
       if(lastFactor == n) {
-         return Arrays.asList(lastFactor);
-      }
-      
-      // if n is divisible to lastFactor add it to the List of factors and recursively po
-      List<Integer> result = new ArrayList<>();
-      if (n % lastFactor == 0) {
-         
          result.add(lastFactor);
-         
-         result.addAll(po(lastFactor, n / lastFactor));
       }
-      
-      // if not; increment the lastFactor by 1 and retry the recursive po
       else {
-         if (lastFactor < n) {
-            return po(++lastFactor, n);
+         // if n is divisible to lastFactor add it to the List of factors and recursively po
+         
+         if (n % lastFactor == 0) {
+            
+            result.add(lastFactor);
+            
+            result.addAll(po(lastFactor, n / lastFactor));
          }
+         
+         // if not; increment the lastFactor by 1 and retry the recursive po
+         else {
+            if (lastFactor < n) {
+               return po(++lastFactor, n);
+            }
+         }
+
       }
 
       return result;

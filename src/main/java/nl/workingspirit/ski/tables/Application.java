89b48c036543895dae9cde703ef1ac1908b060eb
Raymond Loman <raymond@carpago.nl>
2018-12-29 13:16:37 +0100

    Mooi

--- src/main/java/nl/workingspirit/ski/tables/Application.java
+++ src/main/java/nl/workingspirit/ski/tables/Application.java
@@ -7,9 +7,9 @@ public class Application {
    public static void main(String[] args) {
 
       TableTools t = new TableTools();
-      t.setAvailableTables(1, 6, 8, 7);
+      t.setAvailableTables(1, 6, 8, 7, 1, 1);
 
-      List<Integer> result = t.solve(13, false);
+      List<Integer> result = t.solve(16, true);
       System.out.println(result);
 
    }

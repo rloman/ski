950f670917f9777972100b0038c2144b2243216f
Raymond Loman <raymond@carpago.nl>
2019-01-09 14:25:17 +0100

    Other example

--- src/main/java/nl/workingspirit/ski/tables/Application.java
+++ src/main/java/nl/workingspirit/ski/tables/Application.java
@@ -7,9 +7,9 @@ public class Application {
    public static void main(String[] args) {
 
       TableTools t = new TableTools();
-      t.setAvailableTables(1, 6, 8, 7, 1, 1);
+      t.setAvailableTables(1, 6, 8, 7, 1, 1, 2);
 
-      List<Integer> result = t.solve(16, true);
+      List<Integer> result = t.solve(11, true);
       System.out.println(result);
 
    }

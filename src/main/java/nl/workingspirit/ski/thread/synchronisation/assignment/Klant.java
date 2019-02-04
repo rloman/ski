815dacc2482e3f2e8f1dde13aed685dd9a52c1e4
Raymond Loman <raymond@carpago.nl>
2019-02-04 17:48:29 +0100

    Add code for topic: Synchronisation assignment

---
+++ src/main/java/nl/workingspirit/ski/thread/synchronisation/assignment/Klant.java
@@ -0,0 +1,12 @@
+package nl.workingspirit.ski.thread.synchronisation.assignment;
+
+// this klant will buy 10 breads (so start 3 Klants which decrement the voorraad)
+public class Klant extends Thread {
+   
+   private Voorraad voorraad = Voorraad.getInstance();
+   
+   @Override
+   public void run() {
+      // Add your code here!
+   }
+}
---
+++ src/main/java/nl/workingspirit/ski/thread/synchronisation/assignment/Voorraad.java
@@ -0,0 +1,36 @@
+package nl.workingspirit.ski.thread.synchronisation.assignment;
+
+// This class models the voorraad (supply)
+public class Voorraad {
+
+   private static Voorraad instance;
+
+   public static synchronized Voorraad getInstance() {
+
+      if (instance == null) {
+         instance = new Voorraad();
+      }
+
+      return instance;
+
+   }
+
+   private int voorraad;
+
+   private Voorraad() {
+
+   }
+
+   public synchronized void increment() {
+      // Add your code here!
+   }
+
+   public synchronized void decrement() throws InterruptedException {
+     // Add your code here!
+   }
+
+   public int getVoorraad() {
+      return voorraad;
+   }
+
+}

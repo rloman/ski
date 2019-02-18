ac3f821b3e01712b5f6bd9c039cf68ab10a5d20e
Raymond Loman <raymond@carpago.nl>
2019-02-18 14:08:42 +0100

    Add some

---
+++ src/main/java/nl/example/app/math/Application.java
@@ -0,0 +1,22 @@
+package nl.example.app.math;
+
+
+public class Application {
+   
+   public static void main(String[] args) {
+      
+      System.out.println(Math.sqrt(2));
+      
+      System.out.println(Math.random() * 1000);
+      
+      System.out.println(Math.max(15, 45));
+      
+      System.out.println(Math.E);
+      
+      System.out.println(Math.PI);
+      
+      System.out.println(Math.sin(45));
+      
+   }
+
+}
